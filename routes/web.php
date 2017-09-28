59e15a5adb1994f0662c2fa34fbb4324ad288754
Ingrid Ayala <ingridayala_94@hotmail.com>
2017-09-28 13:40:42 -0600

    sexto commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('plantillas.principalc');
+});
+
+Route::resource('login','LoginController');
+Route::resource('usuario','UserController');
+Route::resource('carrera','CarreraController');
+Route::resource('estudiante','EstudianteController');
+Route::resource('bitacora','BitacoraController');
+Route::resource('proyecto','ProyectoController');
+Route::resource('documento','DocumentoController');
+Route::resource('enlace','EnlaceController');
